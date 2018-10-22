855d26083ad69307460c95c4334c313a0389f685
SeraphSarakiel <sebastian.duchardt@gmx.de>
2018-10-22 14:19:48 +0200

    version 0.02

--- src/main/java/ProgrammierProjekt/Navigation/App.java
+++ src/main/java/ProgrammierProjekt/Navigation/App.java
@@ -19,14 +19,52 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
+    	
+    	
     	try {
-			BufferedReader br = getBufferedReaderForCompressedFile("/home/acurazee/Downloads/germany.fmi.bz2");
+		    int intAllg = 0;
+    		BufferedReader br = getBufferedReaderForCompressedFile("/home/acurazee/Downloads/stgtregbz.fmi.bz2");
+			System.out.println(br.readLine());
+			System.out.println(br.readLine());
+			System.out.println(br.readLine());
+			System.out.println(br.readLine());
+			System.out.println(br.readLine());
 			
+			int numberOfNodes = Integer.parseInt(br.readLine());
+			int numberOfEdges = Integer.parseInt(br.readLine());
+			int[] offset = new int[numberOfNodes];
 			
-			for(int i = 0; i < 10; i++) {
-				System.out.println(br.readLine());
+    		double[][] nodes = new double[numberOfNodes][2];
+    		
+    		//src 0, trgt 1, cost 2
+    		int[][] edgeList = new int[numberOfEdges][3]; 
+    		
+			
+    		for(int i = 0; i < numberOfNodes; i++) {
+    		   String input = br.readLine();
+    		   String[] split = input.split(" ");
+    		   nodes[Integer.parseInt((split[0]))][0] = Double.parseDouble(split[2]); 
+    		   nodes[Integer.parseInt((split[0]))][1] = Double.parseDouble(split[3]);
+    		}
+    		
+    		for(int i = 0;i < numberOfEdges; i++) {
+    			String input = br.readLine();
+    			String[] split = input.split(" ");
+    			offset[Integer.parseInt(split[0])]++;
+    			edgeList[i][0] = Integer.parseInt(split[0]);
+    			edgeList[i][1] = Integer.parseInt(split[1]);
+    			edgeList[i][2] = Integer.parseInt(split[2]);
+    			
+    		}
+    		
+    		for(int i = 1; i < offset.length; i++) {
+    		
+    			offset[i] = offset[i] + offset[i - 1];	
+    		}
+			for(int i : offset) {
+				System.out.println(i);
 			}
-			
+    		
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
